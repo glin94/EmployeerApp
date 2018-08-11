70336ad4d7cb0280827fd0a67b0b24fcb919720c
glin94 <narikus94@gmail.com>
2018-08-11 10:23:08 +0300

    Install
    Initall Commit

---
+++ src/components/NewEmployee.vue
@@ -0,0 +1,80 @@
+<template>
+        <div id="new-employee">
+                <h3>New Employee</h3>
+                <div class="row">
+                        <form @submit.prevent="saveEmployee" class="col s12">
+                                 <div class="row">
+                                         <div class="input-field col s12">
+                                                 <input type="text" v-model="employee_id"
+                                                 required>
+                                                 <label>
+                                                         Employee #ID
+                                                 </label>
+                                         </div>
+                                 </div>
+                                  <div class="row">
+                                         <div class="input-field col s12">
+                                                 <input type="text" v-model="name"
+                                                 required>
+                                                 <label>
+                                                         Name
+                                                 </label>
+                                         </div>
+                                 </div>
+                                  <div class="row">
+                                         <div class="input-field col s12">
+                                                 <input type="text" v-model="dept"
+                                                 required>
+                                                 <label>
+                                                         Dept
+                                                 </label>
+                                         </div>
+                                 </div> 
+                                  <div class="row">
+                                         <div class="input-field col s12">
+                                                 <input type="text" v-model="position"
+                                                 required>
+                                                 <label>
+                                                         Position
+                                                 </label>
+                                         </div>
+                                 </div>
+                                 <router-link to="/" class="btn grey" >Cancel</router-link>
+                                 <button type="submit" class="btn blue">Add</button>
+                        </form>
+                </div>
+        </div>
+</template>
+<script>
+import db from "./firebaseInit";
+export default {
+  name: "new-employee",
+  data() {
+    return {
+      employee_id: null,
+      name: null,
+      dept: null,
+      position: null
+    };
+  },
+  methods: {
+    saveEmployee() {
+      const settings = { timestampsInSnapshots: true };
+      db.settings(settings);
+      db
+        .collection("employee")
+        .add({
+          employee_id: this.employee_id,
+          name: this.name,
+          dept: this.dept,
+          position: this.position
+        })
+        .then(docRef => {
+          this.$router.push("/");
+        })
+        .catch(error => console.log(err));
+      M.toast({ html: "Added!" });
+    }
+  }
+};
+</script>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAgX0n5kHH_Xv7qWXovCi_nuo4M2VZGU3s",
+    authDomain: "employeeapp-8783c.firebaseapp.com",
+    databaseURL: "https://employeeapp-8783c.firebaseio.com",
+    projectId: "employeeapp-8783c",
+    storageBucket: "employeeapp-8783c.appspot.com",
+    messagingSenderId: "25748122429"
+}
